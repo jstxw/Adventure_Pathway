b2bdf6fa6eda38f93df56dd4fe23efe5ff68f863
jstxw <ssivfe@Gmail.com>
2025-08-02 02:00:02 -0400

    Backend FASTAPI setup, middleware, env

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(
+    title='FastAPI',
+    description='Adventure_Story',
+    version= '0.1.0',
+    docs_url='/docs',
+    redoc_url='/redoc',
+    )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True, # allow api endpoints to be set
+    allow_methods=["*"], #allow post, get, put
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__": #only run the following if ran only in this file
+    import uvicorn
+    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+
